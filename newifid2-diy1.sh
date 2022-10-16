66e21db9152c833f8362fd7484e0d72bc796f214
acer168 <114073340+acer168@users.noreply.github.com>
2022-10-16 21:31:34 +0800

    Update newifid2-diy1.sh

--- newifid2-diy1.sh
+++ newifid2-diy1.sh
@@ -14,5 +14,9 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+echo 'src-git ddnsto https://github.com/linkease/nas-packages' >>feeds.conf.default
+echo 'src-git ddnsto_luci https://github.com/linkease/nas-packages-luci' >>feeds.conf.default
