95d63b0e80af5cbbf68081e84bd5eb89bb33df43
acer168 <114073340+acer168@users.noreply.github.com>
2022-10-17 09:46:21 +0800

    Update newifid2-diy1.sh

--- newifid2-diy1.sh
+++ newifid2-diy1.sh
@@ -17,6 +17,10 @@
 # echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
+cd openwrt
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 echo 'src-git ddnsto https://github.com/linkease/nas-packages' >>feeds.conf.default
 echo 'src-git ddnsto_luci https://github.com/linkease/nas-packages-luci' >>feeds.conf.default
