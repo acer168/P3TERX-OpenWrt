9d62aa7f7121089843f5445ec98a3a0cbc5c5aeb
acer168 <114073340+acer168@users.noreply.github.com>
2022-10-17 09:47:45 +0800

    Update newifid2-diy2.sh

--- newifid2-diy2.sh
+++ newifid2-diy2.sh
@@ -30,7 +30,8 @@ sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
 
 # 默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 修改NAS为网络存储
 sed -i 's/"NAS"/"网络存储"/g' `grep "NAS" -rl ./`
